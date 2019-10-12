4dfa505b7744961301339a13cd459e513e4b4316
ruben <rubenperez038@gmail.com>
2019-10-12 12:07:07 +0100

    Made functions in descriptives more flexible

--- prettyresults/descriptives.py
+++ prettyresults/descriptives.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 
-from .utils import VarType, readable_index, freq_bar
-from prettyresults.utils import freq_pie
+from .utils import VarType, readable_index, freq_bar, freq_pie, format_float
 
 # For simplicity, mean CI is included here too
 def mean_confidence_interval(data, confidence=0.95):
@@ -16,22 +15,29 @@ def mean_confidence_interval(data, confidence=0.95):
     interval_length = t_value * sem
     return (sample_mean, sample_mean-interval_length, sample_mean+interval_length, n)
 
+def format_mean_ci_raw(lower, upper, decimals=2):
+    return '{} ≤ μ ≤ {}'.format(format_float(lower, decimals), format_float(upper, decimals))
+
+def format_mean_ci(result, decimals=2):
+    return format_mean_ci_raw(result[1], result[2], decimals)
+
 # Utilities to add results
-def add_mean_ci_result(parent_result, series, confidence=0.95):
+def add_mean_ci_result(parent_result, series, confidence=0.95,
+                       result_id='mean_ci', result_name='Intervalo de confianza para la media'):
     mean, lower, upper, n = mean_confidence_interval(series.values, confidence)
     total_cases = len(series)
     lost_cases = total_cases - n
     lost_cases_percent = lost_cases/total_cases*100.0
     parent_result.add_keyvalue_table(
-        'mean_ci',
-        'Intervalo de confianza para la media',
+        result_id,
+        result_name,
         [
             ['Tamaño de muestra total', str(total_cases)],
             ['Casos perdidos', '{} ({:.2f}%)'.format(lost_cases, lost_cases_percent)],
             ['Tamaño de muestra efectivo', str(n)],
             ['Confianza', str(confidence)],
             ['Valor estimado para la media', '{:.2f}'.format(mean)],
-            ['Intervalo de confianza', '{:.2f} ≤ μ ≤ {:.2f}'.format(lower, upper)]
+            ['Intervalo de confianza', format_mean_ci_raw(lower, upper)]
         ]
     )
 
@@ -42,11 +48,17 @@ def add_histogram_result(parent_result, series, var_meta):
     plt.title(var_meta['desc'])
     parent_result.add_figure('hist', 'Histograma')
     plt.close('all')
+
     
-def add_frequency_results(parent_result, series, var_meta, *, bar_plot=True, pie_plot=False):
+def add_frequency_results(parent_result, series, var_meta, *, calculate_value_counts=True, bar_plot=True, pie_plot=False):
+    if calculate_value_counts:
+        value_counts = series.value_counts()
+    else:
+        value_counts = series.copy()
+        
     # Pretty value counts
-    value_counts = series.value_counts()
     value_counts.index = readable_index(value_counts.index, var_meta)
+    value_counts.index.name = var_meta['desc']
     
     # Bar plot
     if bar_plot:
@@ -59,10 +71,16 @@ def add_frequency_results(parent_result, series, var_meta, *, bar_plot=True, pie
         parent_result.add_figure('freq_pie', 'Gráfico de frecuencias (diagrama de sectores)')
     
     # Table
-    sample_size = len(series.index)
-    num_nans = series.isna().sum()
-    effective_sample_size = sample_size - num_nans
-    nans_percent = 100.0*num_nans/sample_size
+    effective_sample_size = value_counts.sum()
+    if calculate_value_counts:
+        sample_size = len(series.index)
+        num_nans = series.isna().sum()
+        nans_percent = 100.0*num_nans/sample_size
+        table_post = 'N = {}, perdidos = {} ({:.2f}%)'.format(sample_size, num_nans, nans_percent)
+    else:
+        sample_size = effective_sample_size
+        table_post = 'N = {}'.format(sample_size)
+        
     df_value_counts = pd.DataFrame(data={
         'Frecuencia': value_counts,
         'Porcentaje': value_counts.map(lambda v: '{:.2f}%'.format(100.0*v/effective_sample_size))
@@ -71,7 +89,7 @@ def add_frequency_results(parent_result, series, var_meta, *, bar_plot=True, pie
         'freq_table',
         'Tabla de frecuencias',
         df_value_counts,
-        post='N = {}, perdidos = {} ({:.2f}%)'.format(sample_size, num_nans, nans_percent)
+        post=table_post
     )
     
     plt.close('all')
